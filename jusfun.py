f814a7a5d4fcc38daf0a6c4d9ec82cb94466a881
Manasa950 <manumanasa9015@gmail.com>
2025-08-16 17:58:27 +0530

    if condition

--- jusfun.py
+++ jusfun.py
@@ -1,6 +1,14 @@
 print("Hello World")
 
 for i in range(10,20):
-    print(i)
+    if i % 2 == 0:
+        print(i)
 
-print(i) 
+num = int(input("Enter Number: "))
+for i in range(num):
+    if i %2 == 0:
+     print("even")
+     i += 1
+    else:
+     print("odd")
+     i += 1
